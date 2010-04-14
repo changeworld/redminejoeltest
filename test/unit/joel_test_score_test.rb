e839ba4a6d082d769d6c4991f6e242a5cc1ff5d0
Takashi Takebayashi <changesworlds@gmail.com>
2010-04-14 21:50:31 +0900

    test/unit ???

--- test/unit/joel_test_score_test.rb
+++ test/unit/joel_test_score_test.rb
@@ -1,10 +1,29 @@
+# Joel Test plugin for Redmine
+# Copyright (C) 2010  Takashi Takebayashi
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 require File.dirname(__FILE__) + '/../test_helper'
 
 class JoelTestExamineTest < ActiveSupport::TestCase
   fixtures :joel_test_scores
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_is_joel_test_scores_should_return_nil
+    assert_nil JoelTestScore.find_last_score_of_user(0)
+  end
+
+  def test_is_joel_test_scores_should_return_not_nil
+    assert_not_nil JoelTestScore.find_last_score_of_user(1)
   end
 end
