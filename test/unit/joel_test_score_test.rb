bdf01db5b24ac4f03c4dd3587ca54f40d12ada6d
Takashi Takebayashi <changesworlds@gmail.com>
2010-04-14 23:21:13 +0900

    test/unit ???

--- test/unit/joel_test_score_test.rb
+++ test/unit/joel_test_score_test.rb
@@ -16,14 +16,21 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 require File.dirname(__FILE__) + '/../test_helper'
 
-class JoelTestExamineTest < ActiveSupport::TestCase
+class JoelTestScoreTest < ActiveSupport::TestCase
   fixtures :joel_test_scores
 
   def test_is_joel_test_scores_should_return_nil
-    assert_nil JoelTestScore.find_last_score_of_user(0)
+    assert_nil(JoelTestScore.find_last_score_of_user(0))
   end
 
   def test_is_joel_test_scores_should_return_not_nil
-    assert_not_nil JoelTestScore.find_last_score_of_user(1)
+    assert_not_nil(JoelTestScore.find_last_score_of_user(1))
+    assert_not_nil(JoelTestScore.find_last_score_by_user)
+  end
+
+  def test_is_joel_test_scores_should_equal
+    user = JoelTestScore.find_last_score_of_user(1)
+    assert_equal(1, user.user_id)
+    assert_equal(3, user.score)
   end
 end
